89b7f5b32928313b0ed82826dc85f0a52b03bd78
Robert Fauver <robertfauver@gmail.com>
2015-01-28 11:13:17 -0500

    tweaks

--- scripts.js
+++ scripts.js
@@ -1,43 +1,54 @@
 $(document).ready(function () {
 
-  $('#slide-nav.navbar-inverse').after($('<div class="inverse" id="navbar-height-col"></div>'));
+    var toggler = '.navbar-toggle';
+    var pagewrapper = '#page-content';
+    var navigationwrapper = '.navbar-header';
+    var slidewidth = '80%';
+    var menuneg = '-100%';
+    var slideneg = '-80%';
 
-  $('#slide-nav.navbar-default').after($('<div id="navbar-height-col"></div>'));
-
-  var toggler = '.navbar-toggle';
-  var pagewrapper = '#page-content';
-  var navigationwrapper = '.navbar-header';
-  var menuwidth = '100%'; // the menu inside the slide menu itself
-  var slidewidth = '80%';
-  var menuneg = '-100%';
-  var slideneg = '-80%';
-
-
-  $("#slide-nav").on("click", toggler, function (e) {
-    var selected = $(this).hasClass('slide-active');
-    $('#slidemenu').stop().animate({
-      right: selected ? menuneg : '0px'
+    $("#slide-nav").on("click", toggler, function () {
+      var selected = $(this).hasClass('slide-active');
+      var $slideMenu = $('#slidemenu');
+      $slideMenu.stop().animate({
+        right: selected ? menuneg : '0px'
+      });
+      $('#navbar-height-col').stop().animate({
+        right: selected ? slideneg : '0px'
+      });
+      $(pagewrapper).stop().animate({
+        right: selected ? '0px' : slidewidth
+      });
+      $(navigationwrapper).stop().animate({
+        right: selected ? '0px' : slidewidth
+      });
+      $(this).toggleClass('slide-active', !selected);
+      $slideMenu.toggleClass('slide-active');
+      $('#page-content, .navbar, body, .navbar-header').toggleClass('slide-active');
     });
-    $('#navbar-height-col').stop().animate({
-      right: selected ? slideneg : '0px'
-    });
-    $(pagewrapper).stop().animate({
-      right: selected ? '0px' : slidewidth
-    });
-    $(navigationwrapper).stop().animate({
-      right: selected ? '0px' : slidewidth
-    });
-    $(this).toggleClass('slide-active', !selected);
-    $('#slidemenu').toggleClass('slide-active');
-    $('#page-content, .navbar, body, .navbar-header').toggleClass('slide-active');
-  });
 
-  var selected = '#slidemenu, #page-content, body, .navbar, .navbar-header';
+    $('.close-menu').on('click', function() {
+      $('#slidemenu').stop().animate({
+        right: selected ? menuneg : '-80px'
+      });
+      $('#navbar-height-col').stop().animate({
+        right: selected ? slideneg : '-80px'
+      });
+      $(pagewrapper).stop().animate({
+        right: selected ? '-80px' : slidewidth
+      });
+      $(navigationwrapper).stop().animate({
+        right: selected ? '-80px' : slidewidth
+      });
+      $('#page-content, .navbar, body, .navbar-header, #slidemenu').toggleClass('slide-active');
+    });
 
-  $(window).on("resize", function () {
-    if ($(window).width() > 767 && $('.navbar-toggle').is(':hidden')) {
-      $(selected).removeClass('slide-active');
-    }
-  });
+    var selected = '#slidemenu, #page-content, body, .navbar, .navbar-header';
+
+    $(window).on("resize", function () {
+      if ($(window).width() > 767 && $('.navbar-toggle').is(':hidden')) {
+        $(selected).removeClass('slide-active');
+      }
+    });
 
 });
